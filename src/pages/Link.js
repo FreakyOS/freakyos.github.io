f3dee531ed660adf0491d7e568946dd63deed67e
Niteshkumar <nitesh156200@gmail.com>
2020-07-29 15:58:48 +0530

    FreakySite: Update insta & twitter details
    * Introduce & hide team because it's WIP
    
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>
    Change-Id: I909656906515b7429e8c3267e8813f2b7b960dba

--- src/pages/Link.js
+++ src/pages/Link.js
@@ -6,7 +6,9 @@ import jenkinsIcon from '@iconify/icons-logos/jenkins';
 import paypalIcon from '@iconify/icons-logos/paypal';
 import gerritIcon from '@iconify/icons-simple-icons/gerrit';
 import githubOctocat from '@iconify/icons-logos/github-octocat';
-import telegramPlane from '@iconify/icons-la/telegram-plane';
+import telegramIcon from '@iconify/icons-logos/telegram';
+import instagramIcon from "@iconify/icons-il/instagram";
+import twitterIcon from "@iconify/icons-logos/twitter";
 
 function initializeReactGA() {
     ReactGA.initialize(process.env.REACT_APP_GA_ID);
@@ -16,86 +18,164 @@ function initializeReactGA() {
 export default class Links extends Component{
     render(){
         initializeReactGA();
-        return(
-            <React.Fragment >
-                <div style={{paddingTop:'60px'}}>
-                <Jumbotron fluid >
-                    <Container fluid className="">
+        return (
+          <React.Fragment>
+            <div style={{ paddingTop: "60px" }}>
+              <Jumbotron fluid>
+                <Container fluid className="">
+                  <ListGroup flush>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Development</ListGroupItemHeading>
+                      <ListGroupItemText>
                         <ListGroup flush>
-                            <ListGroupItem>
-                                <ListGroupItemHeading>
-                                    Development
-                                </ListGroupItemHeading>
-                                <ListGroupItemText>
-                                    <ListGroup flush>
-                                        <ListGroupItem tag='a' href="https://github.com/freakyos" action>
-                                            <h6>
-                                                <Icon icon={githubOctocat} width="2.5rem" height="2.5rem" />&nbsp;
-                                                GitHub
-                                            </h6>                                        
-                                        </ListGroupItem>
-                                        <ListGroupItem tag='a' href="http://freakyos.xyz" action>
-                                            <h6>
-                                                <Icon icon={gerritIcon} width="2.5rem" height="2.5rem" />&nbsp;
-                                                Gerrit
-                                            </h6>                                        
-                                        </ListGroupItem>
-                                        <ListGroupItem tag='a' href="http://jenkins.freakyos.xyz" action>
-                                            <h6>
-                                                <Icon icon={jenkinsIcon} width="2.5rem" height="2.5rem" />&nbsp;
-                                                Jenkins
-                                            </h6>                                        
-                                        </ListGroupItem>
-                                    </ListGroup>
-                                </ListGroupItemText>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <ListGroupItemHeading>
-                                    Telegram Groups
-                                </ListGroupItemHeading>
-                                <ListGroupItemText>
-                                    <ListGroup flush>
-                                        <ListGroupItem tag='a' href="https://t.me/freakyos" action>
-                                            <h6>
-                                            <Icon icon={telegramPlane} width="2rem" height="2rem" />&nbsp;
-                                            FreakyOS Townhall
-                                            </h6>                                        
-                                        </ListGroupItem>
-                                        <ListGroupItem tag='a' href="https://t.me/freakyos_bulletin" action>
-                                            <h6>
-                                            <Icon icon={telegramPlane} width="2rem" height="2rem" />&nbsp;
-                                            FreakyOS Bulletin
-                                            </h6>                                        
-                                        </ListGroupItem>
-                                        <ListGroupItem tag='a' href="http://t.me/FreakyOSGraphics" action>
-                                            <h6>
-                                            <Icon icon={telegramPlane} width="2rem" height="2rem" />&nbsp;
-                                            FreakyOS Graphics
-                                            </h6>                                        
-                                        </ListGroupItem>
-                                    </ListGroup>
-                                </ListGroupItemText>
-                            </ListGroupItem>
-                            <ListGroupItem>
-                                <ListGroupItemHeading>
-                                    Donation
-                                </ListGroupItemHeading>
-                                <ListGroupItemText>
-                                    <ListGroup flush>
-                                        <ListGroupItem tag='a' href="https://paypal.me/bunnyy95" action>
-                                            <h5>
-                                                <Icon icon={paypalIcon} width="2rem" height="2rem" />&nbsp;
-                                                PayPal
-                                            </h5>                                        
-                                        </ListGroupItem>
-                                    </ListGroup>
-                                </ListGroupItemText>
-                            </ListGroupItem>
+                          <ListGroupItem
+                            tag="a"
+                            href="https://github.com/freakyos"
+                            action
+                          >
+                            <h6>
+                              <Icon
+                                icon={githubOctocat}
+                                width="2.5rem"
+                                height="2.5rem"
+                              />
+                              &nbsp; GitHub
+                            </h6>
+                          </ListGroupItem>
+                          <ListGroupItem
+                            tag="a"
+                            href="http://freakyos.xyz"
+                            action
+                          >
+                            <h6>
+                              <Icon
+                                icon={gerritIcon}
+                                width="2.5rem"
+                                height="2.5rem"
+                              />
+                              &nbsp; Gerrit
+                            </h6>
+                          </ListGroupItem>
+                          <ListGroupItem
+                            tag="a"
+                            href="http://jenkins.freakyos.xyz"
+                            action
+                          >
+                            <h6>
+                              <Icon
+                                icon={jenkinsIcon}
+                                width="2.5rem"
+                                height="2.5rem"
+                              />
+                              &nbsp; Jenkins
+                            </h6>
+                          </ListGroupItem>
                         </ListGroup>
-                    </Container>
-                </Jumbotron>
-                </div>
-            </React.Fragment>
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Media</ListGroupItemHeading>
+                      <ListGroupItemText>
+                        <ListGroup flush>
+                          <ListGroupItem
+                            tag="a"
+                            href="https://t.me/freakyos"
+                            action
+                          >
+                            <h6>
+                              <Icon
+                                icon={telegramIcon}
+                                width="2rem"
+                                height="2rem"
+                              />
+                              &nbsp; FreakyOS Townhall
+                            </h6>
+                          </ListGroupItem>
+                          <ListGroupItem
+                            tag="a"
+                            href="https://t.me/freakyos_bulletin"
+                            action
+                          >
+                            <h6>
+                              <Icon
+                                icon={telegramIcon}
+                                width="2rem"
+                                height="2rem"
+                              />
+                              &nbsp; FreakyOS Bulletin
+                            </h6>
+                          </ListGroupItem>
+                          <ListGroupItem
+                            tag="a"
+                            href="http://t.me/FreakyOSGraphics"
+                            action
+                          >
+                            <h6>
+                              <Icon
+                                icon={telegramIcon}
+                                width="2rem"
+                                height="2rem"
+                              />
+                              &nbsp; FreakyOS Graphics
+                            </h6>
+                          </ListGroupItem>
+                          <ListGroupItem
+                            tag="a"
+                            href="https://twitter.com/Freaky_OS?s=09"
+                            action
+                          >
+                            <h6>
+                              <Icon
+                                icon={twitterIcon}
+                                width="2rem"
+                                height="2rem"
+                              />
+                              &nbsp; Twitter
+                            </h6>
+                          </ListGroupItem>
+                          <ListGroupItem
+                            tag="a"
+                            href="https://instagram.com/freaky_os?igshid=1bxtruoobuagb"
+                            action
+                          >
+                            <h6>
+                              <Icon
+                                icon={instagramIcon}
+                                width="2rem"
+                                height="2rem"
+                              />
+                              &nbsp; Instagram
+                            </h6>
+                          </ListGroupItem>
+                        </ListGroup>
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <ListGroupItemHeading>Donation</ListGroupItemHeading>
+                      <ListGroupItemText>
+                        <ListGroup flush>
+                          <ListGroupItem
+                            tag="a"
+                            href="https://paypal.me/bunnyy95"
+                            action
+                          >
+                            <h5>
+                              <Icon
+                                icon={paypalIcon}
+                                width="2rem"
+                                height="2rem"
+                              />
+                              &nbsp; PayPal
+                            </h5>
+                          </ListGroupItem>
+                        </ListGroup>
+                      </ListGroupItemText>
+                    </ListGroupItem>
+                  </ListGroup>
+                </Container>
+              </Jumbotron>
+            </div>
+          </React.Fragment>
         );
     }
 }
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,23 +1,86 @@
-import React, { Component } from 'react';
-import ReactGA from 'react-ga';
-import {Container} from 'reactstrap';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import { Container, Row, Col } from "reactstrap";
+import axios from "axios";
+
+const Member = React.lazy(() => import("components/Member"));
 
 function initializeReactGA() {
-    ReactGA.initialize(process.env.REACT_APP_GA_ID);
-    ReactGA.pageview('/downloads');
+  ReactGA.initialize(process.env.REACT_APP_GA_ID);
+  ReactGA.pageview("/Team");
 }
 
-export default class Links extends Component{
-    render(){
-        initializeReactGA();
-        return(
-            <React.Fragment>
-                <Container style={{paddingTop:'60px'}}>
-                    <h1>
-                        Our Freaky Team!
-                    </h1>
-                </Container>
-            </React.Fragment>
-        );
-    }
+export default class Teams extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      team: [],
+      maintainers: [],
+    };
+    this.generateTeam = this.generateTeam.bind(this);
+    this.generateMaintainers = this.generateMaintainers.bind(this);
+  }
+  componentDidMount() {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/FreakyOS/packages_apps_Settings/still_alive/assets/freaky.json"
+      )
+      .then((response) => {
+        this.setState({
+          team: response.data.team,
+          maintainers: response.data.maintainers,
+        });
+      });
+  }
+  generateCard(data) {
+    return data.map((member, i) => {
+      return (
+        <Col
+          xs="6"
+          sm="4"
+          md="3"
+          lg="2"
+          className="p-0"
+          style={{ maxHeight: "340px", width: "auto" }}
+        >
+          <Member key={i} member={member} />
+        </Col>
+      );
+    });
+  }
+  generateTeam() {
+    return (
+      <React.Fragment>
+        <Row className="mx-auto justify-content-center">
+          <h2>Team</h2>
+        </Row>
+        <Row className="mx-auto justify-content-center">
+          {this.generateCard(this.state.team)}
+        </Row>
+      </React.Fragment>
+    );
+  }
+  generateMaintainers() {
+    return (
+      <React.Fragment>
+        <Row className="mx-auto justify-content-center">
+          <h2>Maintainers</h2>
+        </Row>
+        <Row className="mx-auto justify-content-center">
+          {this.generateCard(this.state.maintainers)}
+        </Row>
+      </React.Fragment>
+    );
+  }
+  render() {
+    initializeReactGA();
+    return (
+      <React.Fragment>
+        <Container style={{ paddingTop: "60px" }}>
+          {this.generateTeam()}
+          {this.generateMaintainers()}
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
