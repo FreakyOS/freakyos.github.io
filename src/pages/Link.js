a8fdfc44cae5a92fd9c120dc108453d069d36405
Niteshkumar <nitesh156200@gmail.com>
2020-08-31 17:21:23 +0530

    FreakySite: Links: Added corwdin and official links
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>
    Change-Id: Ie7a405522c378dbc63041c371b0f01c1dc16bdd6
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>

--- src/pages/Link.js
+++ src/pages/Link.js
@@ -9,6 +9,7 @@ import githubOctocat from '@iconify/icons-logos/github-octocat';
 import telegramIcon from '@iconify/icons-logos/telegram';
 import instagramIcon from "@iconify/icons-il/instagram";
 import twitterIcon from "@iconify/icons-logos/twitter";
+import fileTypeCrowdin from '@iconify/icons-vscode-icons/file-type-crowdin';
 
 function initializeReactGA() {
     ReactGA.initialize(process.env.REACT_APP_GA_ID);
@@ -19,86 +20,200 @@ export default class Links extends Component {
   render() {
     initializeReactGA();
     return (
-      <React.Fragment >
-        <Container fluid className='m-1 d-flex flex-column align-items-center justify-content-center'>
-          <Row className='d-flex align-items-center justify-content-center'>
-            <Col xs='10' sm='5' md='4' xl='4' className='m-1 link-group rounded d-flex flex-column align-items-center justify-content-center'>
-              <span className='m-2' >Contribute</span>
-              <div className='m-1 d-flex justify-content-center'>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a href='https://github.com/freakyos' rel="noopener noreferrer" target='_blank'>
+      <React.Fragment>
+        <Container
+          fluid
+          className="m-1 d-flex flex-column align-items-center justify-content-center"
+        >
+          <Row className="d-flex align-items-center justify-content-center">
+            <Col
+              xs="10"
+              sm="5"
+              md="4"
+              xl="4"
+              className="m-1 link-group rounded d-flex flex-column align-items-center justify-content-center"
+            >
+              <span className="m-2">Contribute</span>
+              <div className="m-1 d-flex justify-content-center">
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    href="https://github.com/freakyos"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={githubOctocat} width="2.5em" height="2.5em" />
                   </a>
-                  <span className='link-name'>GitHub</span>
+                  <span className="link-name">GitHub</span>
                 </div>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a href='http://freakyos.xyz' rel="noopener noreferrer" target='_blank'>
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    href="http://freakyos.xyz"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={gerritIcon} width="2.5rem" height="2.5rem" />
                   </a>
-                  <span className='link-name'>Gerrit</span>
+                  <span className="link-name">Gerrit</span>
                 </div>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a href='http://jenkins.freakyos.xyz' rel="noopener noreferrer" target='_blank'>
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    href="http://jenkins.freakyos.xyz"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={jenkinsIcon} width="2.5rem" height="2.5rem" />
                   </a>
-                  <span className='link-name'>Jenkins</span>
+                  <span className="link-name">Jenkins</span>
                 </div>
               </div>
             </Col>
-            <Col xs='10' sm='5' md='4' xl='4' className='m-1 link-group rounded d-flex flex-column align-items-center justify-content-center'>
-              <span className='m-2' >Talk to us!</span>
-              <div className='m-1 d-flex justify-content-center'>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a className='' href='https://t.me/freakyos' rel="noopener noreferrer" target='_blank'>
+            <Col
+              xs="10"
+              sm="5"
+              md="4"
+              xl="4"
+              className="m-1 link-group rounded d-flex flex-column align-items-center justify-content-center"
+            >
+              <span className="m-2">Talk to us!</span>
+              <div className="m-1 d-flex justify-content-center">
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    className=""
+                    href="https://t.me/freakyos"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={telegramIcon} width="2.5em" height="2.5em" />
                   </a>
-                  <span className='link-name'>Townhall</span>
+                  <span className="link-name">Townhall</span>
                 </div>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a href='https://t.me/freakyos_bulletin' rel="noopener noreferrer" target='_blank'>
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    href="https://t.me/freakyos_bulletin"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={telegramIcon} width="2.5rem" height="2.5rem" />
                   </a>
-                  <span className='link-name'>Bulletin</span>
+                  <span className="link-name">Bulletin</span>
                 </div>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a href='http://t.me/FreakyOSGraphics' rel="noopener noreferrer" target='_blank'>
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    href="http://t.me/FreakyOSGraphics"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={telegramIcon} width="2.5rem" height="2.5rem" />
                   </a>
-                  <span className='link-name'>Graphics!</span>
+                  <span className="link-name">Graphics!</span>
                 </div>
               </div>
             </Col>
-            <Col xs='10' sm='5' md='4' xl='4' className='m-1 link-group rounded d-flex flex-column align-items-center justify-content-center'>
-              <span className='m-2' >Our Social!</span>
-              <div className='m-1 d-flex justify-content-center'>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a href='https://instagram.com/freaky_os?igshid=1bxtruoobuagb' rel="noopener noreferrer" target='_blank'>
+            <Col
+              xs="10"
+              sm="5"
+              md="4"
+              xl="4"
+              className="m-1 link-group rounded d-flex flex-column align-items-center justify-content-center"
+            >
+              <span className="m-2">Our Social!</span>
+              <div className="m-1 d-flex justify-content-center">
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    href="https://instagram.com/freaky_os?igshid=1bxtruoobuagb"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={instagramIcon} width="2.5rem" height="2.5rem" />
                   </a>
-                  <span className='link-name'>Instagram</span>
+                  <span className="link-name">Instagram</span>
                 </div>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a href='https://twitter.com/Freaky_OS?s=09' rel="noopener noreferrer" target='_blank'>
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    href="https://twitter.com/Freaky_OS?s=09"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={twitterIcon} width="2.5rem" height="2.5rem" />
                   </a>
-                  <span className='link-name'>Twitter</span>
+                  <span className="link-name">Twitter</span>
                 </div>
               </div>
             </Col>
-            <Col xs='10' sm='5' md='4' xl='3' className='link-group m-1 rounded d-flex flex-column align-items-center justify-content-center'>
-              <span className='m-2' >Support us :)</span>
-              <div className='m-1 d-flex justify-content-center'>
-                <div className='m-2 link-container d-flex flex-column justify-content-center align-items-center'>
-                  <a className='' href='https://paypal.me/bunnyy95' rel="noopener noreferrer" target='_blank'>
+            <Col
+              xs="10"
+              sm="5"
+              md="4"
+              xl="3"
+              className="link-group m-1 rounded d-flex flex-column align-items-center justify-content-center"
+            >
+              <span className="m-2">Support us :)</span>
+              <div className="m-1 d-flex justify-content-center">
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    className=""
+                    href="https://paypal.me/bunnyy95"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
                     <Icon icon={paypalIcon} width="2.5em" height="2.5em" />
                   </a>
-                  <span className='link-name'>Paypal</span>
+                  <span className="link-name">Paypal</span>
+                </div>
+              </div>
+            </Col>
+            <Col
+              xs="10"
+              sm="5"
+              md="4"
+              xl="3"
+              className="link-group m-1 rounded d-flex flex-column align-items-center justify-content-center"
+            >
+              <span className="m-2">Help us with localization!</span>
+              <div className="m-1 d-flex justify-content-center">
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    className=""
+                    href="https://crwd.in/freakyos"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Icon icon={fileTypeCrowdin} width="2.5em" height="2.5em" />
+                  </a>
+                  <span className="link-name">Crowdin</span>
+                </div>
+              </div>
+            </Col>
+            <Col
+              xs="10"
+              sm="5"
+              md="4"
+              xl="3"
+              className="link-group m-1 rounded d-flex flex-column align-items-center justify-content-center"
+            >
+              <span className="m-2">Wanna Apply Official?</span>
+              <div className="m-1 d-flex justify-content-center">
+                <div className="m-2 link-container d-flex flex-column justify-content-center align-items-center">
+                  <a
+                    className="d-flex justify-content-center"
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSdpCi2BOfHO4gGa561oU-14FUiuKS2wgfhoVFgLIkE-ltvYSA/viewform"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      src="https://avatars2.githubusercontent.com/u/65819960?s=200&v=4"
+                      alt="logo"
+                      width="40%"
+                      height="auto"
+                    />
+                  </a>
+                  <span className="link-name">FreakyOS Official</span>
                 </div>
               </div>
             </Col>
           </Row>
         </Container>
-      </React.Fragment >
+      </React.Fragment>
     );
   }
 }
