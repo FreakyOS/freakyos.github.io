5994c0631a24b714f505f3ad0dc4e0910f289d75
Niteshkumar <nitesh156200@gmail.com>
2020-11-20 10:31:00 +0530

    FreakySite: Home Page Improvements!
    Signed-off-by: Niteshkumar <nitesh156200@gmail.com>
    Change-Id: I6348b1f1bdf03a8771532175b641bd470fce80b9

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Icon } from "@iconify/react";
-import flagForIndia from "@iconify/icons-emojione-v1/flag-for-india";
 
 export default class Footer extends Component {
   render() {
@@ -8,11 +6,8 @@ export default class Footer extends Component {
       <React.Fragment>
         <div className="footer fixed-bottom freaky-footer">
           <div className='container text-muted d-flex flex-column justify-content-center align-items-center'>
-            <p>Made with ❤ in {" "}
-              <span>
-                <Icon icon={flagForIndia} width="2rem" height="2rem" />
-              </span>
-              <span>{" "} | Powered by &copy; TheFreaks</span>
+            <p>
+              Made with ❤ in India | Powered by &copy; TheFreaks
             </p>
           </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,102 +1,112 @@
 import React, { Component } from "react";
 import ReactGA from 'react-ga';
-// import banner from 'assets/images/banner.jpg';
-// import {
-//   Jumbotron,
-//   Container,
-//   UncontrolledCarousel
-// } from "reactstrap";
-// import freaky_01 from 'assets/images/freaky_01.png';
-// import freaky_02 from "assets/images/freaky_02.png";
-// import freaky_03 from "assets/images/freaky_03.png";
-// import freaky_04 from 'assets/images/freaky_04.png';
-// import freaky_05 from "assets/images/freaky_05.png";
-// import freaky_06 from 'assets/images/freaky_06.png';
-// import freaky_07 from "assets/images/freaky_07.png";
-// import freaky_08 from "assets/images/freaky_08.png";
-// import freaky_09 from "assets/images/freaky_09.png";
-// import freaky_10 from "assets/images/freaky_10.png";
+import Gallery from 'react-photo-gallery';
+import freaky_01 from 'assets/images/freaky_01.png';
+import freaky_02 from "assets/images/freaky_02.png";
+import freaky_03 from "assets/images/freaky_03.png";
+import freaky_04 from 'assets/images/freaky_04.png';
+import freaky_05 from "assets/images/freaky_05.png";
+import freaky_06 from 'assets/images/freaky_06.png';
+import freaky_07 from "assets/images/freaky_07.png";
+import freaky_08 from "assets/images/freaky_08.png";
+import freaky_09 from "assets/images/freaky_09.png";
+import freaky_10 from "assets/images/freaky_10.png";
+
 function initializeReactGA() {
   ReactGA.initialize(process.env.REACT_APP_GA_ID);
   ReactGA.pageview('/');
 }
 
-// const item = [
-//   {
-//     src: freaky_03,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_02,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_01,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_04,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_07,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_06,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_05,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_08,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_09,
-//     altText: "",
-//     caption: "",
-//   },
-//   {
-//     src: freaky_10,
-//     altText: "",
-//     caption: "",
-//   }
-// ];
+ const images = [
+   {
+     src: freaky_03,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_02,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_01,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_04,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_07,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_06,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_05,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_08,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_09,
+     width: 2,
+     height: 2
+   },
+   {
+     src: freaky_10,
+     width: 2,
+     height: 2
+   }
+ ];
 
 export default class Home extends Component {
   render() {
     initializeReactGA();
     return (
       <React.Fragment>
+        <h1 className="m-10 d-flex justify-content-center">FreakyOS</h1>
+        <div className="mt-10 d-flex flex-column align-items-center">
+          <div class="p-10 w-70 bg-gray-900 rounded">
+          <p className="md:text-centre md:text-xl sm:text-sm">
+            If you're a Freak! Then you're at the right place. Checkout our
+            custom rom and feel The Freakiness!
+          </p>
+        </div>
+        </div>
+        <h1 className="m-10 d-flex justify-content-center">About Us</h1>
+        <div className="flex justify-center">
+          <div className="p-10 w-50 bg-gray-900 rounded">
+          <p className="text-center md:text-xl sm:text-sm">
+            Freaky OS is AOSP based Custom ROM, primarily focused to deliver highly optimized operating system with minimal customization to the end user.
+          </p>
+        </div>
+        </div>
+        <h1 className="m-10 d-flex justify-content-center">What about Updates?</h1>
+        <div className="flex justify-center">
+          <div className="p-10 w-50 bg-gray-900 rounded">
+          <p className="text-center md:text-xl sm:text-sm">
+            Freaky OS team makes sure the users get every month security patch and feature updates and we have our dedicated updater app which keeps your device up to date.
+          </p>
+        </div>
+        </div>
+        <div className="p-5 d-flex flex-column align-items-center">
+          <h1 className="d-flex justify-content-center">
+            Screenshots!
+          </h1>
+        </div>
         <div>
-          {/* <Jumbotron fluid>
-            <Container className="w-100"> */}
-          <div class="p-5 d-flex flex-column align-items-center">
-            <h1 className="display-5">FreakyOS</h1>
-            <p>
-              If you're a Freak! Then you're at the right place. Checkout our
-              custom rom and feel The Freakiness!
-            </p>
-          </div>
-          <h3 class="p-5 d-flex flex-column align-items-center">
-            {/* Checkout some of our screenshots! */}
-          </h3>
-          {/* <div className="d-flex justify-content-center" style={{height:"auto", width:"300px"}}>
-            <UncontrolledCarousel items={item} />
-          </div> */}
-          {/* </Container>
-          </Jumbotron> */}
+          <Gallery photos={images} />
         </div>
       </React.Fragment>
     );
