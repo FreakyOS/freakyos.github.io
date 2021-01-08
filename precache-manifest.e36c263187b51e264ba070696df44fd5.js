0d7bb6491aea1c1abd3e2bf75b67ef2ff2f8b3a5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-08 05:38:16 +0000

    Deploy FreakyOS/freakyos.github.io to github.com/FreakyOS/freakyos.github.io.git:master

---
+++ precache-manifest.e36c263187b51e264ba070696df44fd5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1da1c1a22c2979af6af5b1dc2ba93b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7477f7927c378460f372",
+    "url": "/static/css/main.f493a1d6.chunk.css"
+  },
+  {
+    "revision": "66e4a12c884659107fb2",
+    "url": "/static/js/0.b7f97a1f.chunk.js"
+  },
+  {
+    "revision": "5caf02a5c025826fc91f",
+    "url": "/static/js/10.1637de24.chunk.js"
+  },
+  {
+    "revision": "7b461e7bfee40c7a57a5",
+    "url": "/static/js/11.b1d8bfa9.chunk.js"
+  },
+  {
+    "revision": "b52f1767a54eec677338",
+    "url": "/static/js/12.36eb8976.chunk.js"
+  },
+  {
+    "revision": "5b03208b8958761ae5d1",
+    "url": "/static/js/13.055721c8.chunk.js"
+  },
+  {
+    "revision": "e96f4b8c6d43fc906bf2",
+    "url": "/static/js/14.6aa43dff.chunk.js"
+  },
+  {
+    "revision": "27e849752d71c63eb5ab",
+    "url": "/static/js/15.028e83c8.chunk.js"
+  },
+  {
+    "revision": "1d64004e7a5ab1bee64d",
+    "url": "/static/js/3.2577be53.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.2577be53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c25f6454c8fa73ec389",
+    "url": "/static/js/4.6e6e92cb.chunk.js"
+  },
+  {
+    "revision": "db7bad0b88c98f9e2555",
+    "url": "/static/js/5.e38d6608.chunk.js"
+  },
+  {
+    "revision": "781cf237f285e61d82e8",
+    "url": "/static/js/6.2e310ae7.chunk.js"
+  },
+  {
+    "revision": "b7c3aff21935a7f2dfa4",
+    "url": "/static/js/7.18c9a19e.chunk.js"
+  },
+  {
+    "revision": "727c5856c313cf0a7e9f",
+    "url": "/static/js/8.0fc728a5.chunk.js"
+  },
+  {
+    "revision": "98ab9bdc8bb5a25d1cc9",
+    "url": "/static/js/9.300e5587.chunk.js"
+  },
+  {
+    "revision": "7477f7927c378460f372",
+    "url": "/static/js/main.b59cb90c.chunk.js"
+  },
+  {
+    "revision": "7195930addc3be7e4efa",
+    "url": "/static/js/runtime-main.9e320e24.js"
+  },
+  {
+    "revision": "f37e281f41845c6676e3c8577de009b4",
+    "url": "/static/media/freaky_01.f37e281f.png"
+  },
+  {
+    "revision": "138d58cb8b403befdcaf75a8a1a36590",
+    "url": "/static/media/freaky_02.138d58cb.png"
+  },
+  {
+    "revision": "df7eeb036e12edb126f3172391ef8bca",
+    "url": "/static/media/freaky_03.df7eeb03.png"
+  },
+  {
+    "revision": "c2ab8d5b18fb4e4807c8c2c1446b8f15",
+    "url": "/static/media/freaky_04.c2ab8d5b.png"
+  },
+  {
+    "revision": "5e07ca7fb08add5062d45de7a3b8d398",
+    "url": "/static/media/freaky_05.5e07ca7f.png"
+  },
+  {
+    "revision": "0a2f397e5b0ac0c1a2bba0393fd8a685",
+    "url": "/static/media/freaky_06.0a2f397e.png"
+  },
+  {
+    "revision": "fe1488a76acd98a180d6a352ae9f2c4a",
+    "url": "/static/media/freaky_07.fe1488a7.png"
+  },
+  {
+    "revision": "7e861d7df77ec0d22f1130dbb282c668",
+    "url": "/static/media/freaky_08.7e861d7d.png"
+  },
+  {
+    "revision": "21e9a85b6585f056e232bd35a0314865",
+    "url": "/static/media/freaky_09.21e9a85b.png"
+  },
+  {
+    "revision": "3ca2efabdb766b76bf26d8213bcd5191",
+    "url": "/static/media/freaky_10.3ca2efab.png"
+  },
+  {
+    "revision": "9044b57d2fb44e2725a4bb6d75344d60",
+    "url": "/static/media/logo192.9044b57d.png"
+  }
+]);
